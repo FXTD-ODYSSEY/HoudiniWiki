ca5c9b4d65cb8ed731fc31aff691d72a7b9d1b4a
FXTD-ODYSSEY <FXTD-ODYSSEY@users.noreply.github.com>
2021-01-19 15:03:33 +0000

    deploy: d3910a52eb7240ad8cf75ef749e8add7008481a4

---
+++ js/main.min.afce85847e91824ba8b56d1d51f8464692c29a510b37832a4943f193155e3796.js
@@ -0,0 +1,3 @@
+$(".menu__title").each(function(){$(this).attr("title",$(this).text())
+$(".menu").tooltip({track:true,})});$(".related").tooltip({track:true,})
+$(".link_container").each(function(){$(this)})
