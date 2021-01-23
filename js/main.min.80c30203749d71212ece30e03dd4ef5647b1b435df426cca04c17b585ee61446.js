75318ddd5bef491d43e6dbcfc82477ba4ed637ee
FXTD-ODYSSEY <FXTD-ODYSSEY@users.noreply.github.com>
2021-01-23 07:27:10 +0000

    deploy: 0aeabee41a7e2fb8de6113d0b5d91088e66c49fc

---
+++ js/main.min.80c30203749d71212ece30e03dd4ef5647b1b435df426cca04c17b585ee61446.js
@@ -0,0 +1,12 @@
+$(".menu__title").each(function(){$(this).attr("title",$(this).text())
+$(".menu").tooltip({track:true,})});$(".related").tooltip({track:true,})
+$(".link_container").each(function(){$(this)})
+var name="FXTD-odyssey";var repository="HoudiniWiki";var branch="gh-pages"
+var link="//cdn.jsdelivr.net/gh/"+name+"/"+repository+"@"+branch
+var host=window.location.host
+if(host.startsWith("localhost")){$("img").each(function(){src=$(this).attr("src")
+$(this).attr("src",src.replace(link,host))
+src=$(this).attr("src")})
+$("video").each(function(){src=$(this).attr("src")
+$(this).attr("src",src.replace(link,host))
+src=$(this).attr("src")})}
