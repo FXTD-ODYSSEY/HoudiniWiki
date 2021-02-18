f03ebc6eac5f6b021eaa39610a9cd61ac094d59d
FXTD-ODYSSEY <FXTD-ODYSSEY@users.noreply.github.com>
2021-02-18 07:35:51 +0000

    deploy: 8f9608cc9aa4199efc2cb44d2d61cae5eafaa6c6

---
+++ js/main.min.cd276cabc8180e26ac7eed1f73f4bcb6a0ab882e6c2a3fde87ef2c18d5c29d6e.js
@@ -0,0 +1,34 @@
+function UrlExists(url,cb){jQuery.ajax({url:url,dataType:'text',type:'GET',complete:function(xhr){if(typeof cb==='function')
+cb.apply(this,[xhr.status]);}});}
+_tooltip_func=function(index,elem){description=$(elem).attr("description")
+$(elem).attr("title",$(elem).text())
+this.text=$(elem).text()+"<br>"+description
+href=$(this).attr("href")
+if(!href)return
+links=href.split("/")
+post_id=links[links.length-2]
+host=window.location.host
+this.cover="//"+host+"/posts/"+post_id+"/cover.png"
+fn=function(xhr){status=xhr.status
+content=status==200?'<img src="'+this.cover+'" />'+this.text:this.text
+$(this).tooltip({content,track:true,})}
+$.ajax({url:this.cover,dataType:'text',type:'GET',complete:fn.bind(this)});}
+$(".menu__title").each(_tooltip_func);$(".menu__title--collapse-text").each(_tooltip_func)
+$(".related").tooltip({track:true,})
+$(".link_container").each(function(){$(this)})
+var name="FXTD-ODYSSEY";var repository="HoudiniWiki";var branch="gh-pages"
+var link="cdn.jsdelivr.net/gh/"+name+"/"+repository+"@"+branch
+var host=window.location.host
+var href=window.location.href
+if(host.startsWith("localhost")||href.startsWith("file")){$("img").each(function(){src=$(this).attr("src")
+src=src.replace(link,host)
+src=src.replace("https","http")
+$(this).attr("src",src)
+src=$(this).attr("data-src")
+if(src){src=src.replace(link,host)
+src=src.replace("https","http")
+$(this).attr("data-src",src)}})
+$("video").each(function(){src=$(this).attr("src")
+src=src.replace(link,host).replace("https","http")
+$(this).attr("src",src)})}
+lazyload();
